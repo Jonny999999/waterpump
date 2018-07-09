5bdc23fabb91966631809ff5a01c77e79e9479fb
Rainer <rainer@wwad.de>
2018-07-09 16:35:18 +0200

    start regulate code

--- sw/main.cpp
+++ sw/main.cpp
@@ -20,6 +20,114 @@ extern "C" {
 #undef FILEID
 #define FILEID 0
 
+template <uint16_t n>
+class MovingAverage
+{
+  public:
+  MovingAverage(uint16_t initVal = 0)
+  {
+    for(uint16_t i =0; i<n; i++)
+    {
+      store[i] = initVal;
+    }
+  }
+
+  void add(uint16_t value)
+  {
+    store[stop] = value;
+    stop++;
+    if(stop >= n)
+    {
+      stop = 0;
+    }
+  }
+
+  uint16_t getAverage()
+  {
+    uint32_t sum = 0;
+    for(uint16_t i =0; i<n; i++)
+    {
+      sum += store[i];
+    }
+    uint32_t result = sum / n;
+    return result;
+  }
+
+  private:
+    uint16_t store[n];
+    uint16_t stop = 0;
+};
+
+int vfd_setMode(bool running)
+{
+  uint8_t buf[2];
+  buf[0] = 0;
+  if(running)
+  {
+    buf[1] = 0x02;
+  }
+  else
+  {
+    buf[1] = 0x01;
+  }
+  int rc = modbus_writeRegister(0x2000, buf, 2);
+  return rc;
+
+}
+
+int vfd_setFrequency(uint16_t centiHz)
+{
+  uint16_t freq = centiHz;
+  uint8_t buf[2];
+  if(freq > 6000)
+  {
+    freq = 6000;
+  }
+  if(freq < 400)
+  {
+    //cmd = 0x02;
+    freq = 400;
+  }
+  buf[0] = freq>>8;
+  buf[1] = freq & 0xff;
+  //buf[1] = (buf[1] & (~0x03)) | cmd;
+  int rc = modbus_writeRegister(0x2001, buf, 2);
+  return rc;
+}
+
+const uint16_t targetBarDivider = 2;
+
+void regulateFrequency(MovingAverage<50> & druckAdc, uint16_t targetVal)
+{
+  targetVal = targetVal/targetBarDivider;
+
+}
+
+bool isOff = true;
+void setFrequencyByPoti(uint16_t potiVal)
+{
+  uint16_t freq = potiVal * 6;
+
+  if(freq > 6000)
+  {
+    freq = 6000;
+  }
+  if(freq < 400)
+  {
+    freq = 400;
+    vfd_setMode(false);
+    isOff = true;
+  }
+  else
+  {
+    if(isOff)
+    {
+      vfd_setMode(true);
+      isOff = false;
+    }
+  }
+  vfd_setFrequency(freq);
+}
 
 int main(void)
 {
@@ -30,69 +138,58 @@ int main(void)
   // LED test:
 
   LED0_ON;
-  _delay_ms(500);
+  _delay_ms(125);
   LED1_ON;
-  _delay_ms(500);
+  _delay_ms(125);
   LED2_ON;
-  _delay_ms(500);
+  _delay_ms(125);
+  LED3_ON;
+  _delay_ms(125);
   LED0_OFF;
-  _delay_ms(500);
+  _delay_ms(125);
   LED1_OFF;
-  _delay_ms(500);
+  _delay_ms(125);
   LED2_OFF;
-  _delay_ms(500);
+  _delay_ms(125);
+  LED3_OFF;
+  _delay_ms(125);
 
   uint8_t count = 0;
+
+  uint32_t timeStartShort = time_get();
+  uint32_t timeStartLong = time_get();
+  MovingAverage<50> druckAdcAvg(1023);
   while(1)
   {
     count++;
+
+    if(time_delta(time_get(), timeStartShort) > 10)
+    {
+      timeStartShort = time_get();
+      // 10ms timer
+      
+      druckAdcAvg.add(ReadChannel(ADRUCK));
+    }
+
+    if(time_delta(time_get(), timeStartLong) > 500)
+    {
+      timeStartLong = time_get();
+      // 500ms timer
+      
+      if(T1_IS_ON)
+      {
+        LED1_ON;
+        regulateFrequency(druckAdcAvg, ReadChannel(APOTI));
+      }
+      else
+      {
+        LED1_OFF;
+        setFrequencyByPoti(ReadChannel(APOTI));
+
+      }
+      
+    }
     uint16_t potiVal = ReadChannel(APOTI);
-    uint16_t druckVal = ReadChannel(ADRUCK);
-
-    if(druckVal > potiVal )
-    {
-      //LED2_ON;
-
-    }
-    else
-    {
-      //LED2_OFF;
-
-    }
-    //while(potiVal--)
-    //{
-    //  _delay_ms(2);
-    //}
-    //LEDP_TOGGLE;
-    //LED0_TOGGLE;
-
-    _delay_ms(500);
-
-
-    uint8_t mode[4] = {0,0,0x01,0};
-    int rc = modbus_readRegister(0x2000, mode, 2);
-
-    uint8_t cmd = 0;
-    if(rc ==0)
-    {
-      cmd = 0x02;
-      LED2_ON;
-      LED1_OFF;
-      _delay_ms(100);
-      uint16_t freq = potiVal * 6;
-      if(freq > 6000)
-      {
-        freq = 6000;
-      }
-      if(freq < 500)
-      {
-        freq = 500;
-      }
-      mode[2] = freq>>8;
-      mode[3] = freq & 0xff;
-      mode[1] = (mode[1] & (~0x03)) | cmd;
-      rc = modbus_writeRegister(0x2000, mode, 4);
-    }
   }
 
   return 0;
